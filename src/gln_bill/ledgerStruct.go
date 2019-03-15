c94dcdd7d6809c0013ab820ed55afb1e6c08f8eb
e933715 <e933715>
2019-03-15 16:57:37 +0900

    modify gln_bill ledger

--- src/gln_bill/ledgerStruct.go
+++ src/gln_bill/ledgerStruct.go
@@ -3,28 +3,29 @@ package main
 //Settlement Log Ledger Struct
 
 type glnbill struct {
-	AdjReqNo          string  `json:"ADJ_REQ_NO"`
-	SndrLocalGlnCd    string  `json:"SNDR_LOCAL_GLN_CD"`
-	RcvrLocalGlnCd    string  `json:"RCVR_LOCAL_GLN_CD"`
-	AdjSDt            string  `json:"ADJ_S_DT"`
-	AdjEDt            string  `json:"ADJ_E_DT"`
-	AdjTxCnt          uint64  `json:"ADJ_TX_CNT"`
-	AdjDt             string  `json:"ADJ_DT"`
-	TxPrcpSum         float64 `json:"TX_PRCP_SUM"`
-	RcvrAdjAmt        float64 `json:"RCVR_ADJ_AMT"`
-	GlnAdjAmt         float64 `json:"GLN_ADJ_AMT"`
-	SndrRcvgFeSum     float64 `json:"SNDR_RCVG_FE_SUM"`
-	RcvrRcvgFeSum     float64 `json:"RCVR_RCVG_FE_SUM"`
-	GlnRcvgFeSum      float64 `json:"GLN_RCVG_FE_SUM"`
-	GlnAdjBnkSwiftCd  string  `json:"GLN_ADJ_BNK_SWIFT_CD"`
-	GlnAdjBnkNm       string  `json:"GLN_ADJ_BNK_NM"`
-	GlnAdjAcNo        string  `json:"GLN_ADJ_AC_NO"`
-	RcvrAdjBnkSwiftCd string  `json:"RCVR_ADJ_BNK_SWIFT_CD"`
-	RcvrAdjBnkNm      string  `json:"RCVR_ADJ_BNK_NM"`
-	RcvrAdjAcNo       string  `json:"RCVR_ADJ_AC_NO"`
-	SndrAdjDfnYn      string  `json:"SNDR_ADJ_DFN_YN"`
-	RcvrAdjDfnYn      string  `json:"RCVR_ADJ_DFN_YN"`
-	Txid              string  `json:"TX_ID"`
+	AdjReqNo          string `json:"ADJ_REQ_NO"`
+	SndrLocalGlnCd    string `json:"LOCAL_GLN_CD"`
+	AdjSDt            string `json:"ADJ_S_DT"`
+	AdjEDt            string `json:"ADJ_E_DT"`
+	AdjTxCnt          uint64 `json:"ADJ_TX_CNT"`
+	AdjDt             string `json:"ADJ_DT"`
+	TxPrcpSum         string `json:"TX_PRCP_SUM"`
+	TxPrcpSumSign     string `json:"TX_PRCP_SUM_SIGN"`
+	RcvrAdjAmt        string `json:"RCVR_ADJ_AMT"`
+	RcvrAdjAmtSign    string `json:"RCVR_ADJ_AMT_SIGN"`
+	GlnAdjAmt         string `json:"GLN_ADJ_AMT"`
+	GlnAdjAmtSign     string `json:"GLN_ADJ_AMT_SIGN"`
+	SndrRcvgFeSum     string `json:"SNDR_RCVG_FE_SUM"`
+	SndrRcvgFeSumSign string `json:"SNDR_RCVG_FE_SUM_SIGN"`
+	RcvrRcvgFeSum     string `json:"RCVR_RCVG_FE_SUM"`
+	RcvrRcvgFeSumSign string `json:"RCVR_RCVG_FE_SUM_SIGN"`
+	GlnRcvgFeSum      string `json:"GLN_RCVG_FE_SUM"`
+	GlnRcvgFeSumSign  string `json:"GLN_RCVG_FE_SUM_SIGN"`
+	GlnAdjBnkSwiftCd  string `json:"GLN_ADJ_BNK_SWIFT_CD"`
+	GlnAdjBnkNm       string `json:"GLN_ADJ_BNK_NM"`
+	GlnAdjAcNo        string `json:"GLN_ADJ_AC_NO"`
+	SndrAdjDfnYn      string `json:"LOCAL_GLN_ADJ_DFN_YN"`
+	Txid              string `json:"TX_ID"`
 }
 
 // Query JSON struct
@@ -44,6 +45,3 @@ type hEvt struct {
 	Target []string
 	Data   interface{}
 }
-
-const Receiver string = "RCVR_LOCAL_GLN_CD" //
-const Sender string = "SNDR_LOCAL_GLN_CD"   //
